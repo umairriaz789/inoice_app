98d9ee15c977c124a7983be5dc2dad2022a05c91
umairriaz789 <49087501+umairriaz789@users.noreply.github.com>
2022-01-12 01:37:52 +0500

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,7 +7,8 @@ export default function Header(){
         <>
           <header className="flex flex-col items-center justify-center mb-5 xl:flex-row xl:justify-between">
           <div>
-          <h1 className="text-red-800 font-bold uppercase tracking-wide text-5xl mb-3"> Invoice </h1>
+          <h1 className="text-red-800 font-bold uppercase tracking-wide text-5xl mb-1"> Invoice </h1>
+          <h2 className="uppercase tracking-wide "> E-COMMERCE </h2>
           </div>
           {/* <div>
           <ul className="flex items-center justify-between flex-wrap">
